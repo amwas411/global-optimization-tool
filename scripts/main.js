7c1ded9d5a04bf48af9daebf27be7cd901fdd13e
pavelyarkov <68156110+pavelyarkov@users.noreply.github.com>
2020-12-26 16:31:20 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+functionDimension = 2;
 function DefaultFunc(x1,x2) {
     return 18 * x1 * x1 - 18 * x1 - 12 * x1 * x2 + 8 * x2 * x2 - 12 * x2;
 }
@@ -11,7 +12,7 @@ function BillsFunc(x1,x2) {
 }
 function VittesFunc(x1,x2) {
     let e = Math.exp(20*(x2-x1));
-    let o =Math.pow(10,-4)*(x1-3)*(x1-3)-(x2-x1);
+    let o = Math.pow(10,-4)*(x1-3)*(x1-3)-(x2-x1);
     return o+e;
 }
 function RosenbrockFunc(x1,x2){
@@ -24,7 +25,8 @@ function HookeJeeves (x1,x2,mx,f) {
     minY.push(x2);
     let n=2;
     let functionIter = 0;
-    let y=f(x1,x2);
+    let x = {x1:x1, x2: x2};
+    let y=f.evaluate(x);
     functionIter++;
     let methodIter = 0;
     let d=1;
@@ -38,7 +40,8 @@ function HookeJeeves (x1,x2,mx,f) {
                 sign=-1;
             }
             x1=x1+sign*d;
-            let fX=f(x1,x2);
+            x = {x1:x1, x2: x2};
+            let fX=f.evaluate(x);
             functionIter++;
             if (fX < y){
                 y=fX;
@@ -56,7 +59,8 @@ function HookeJeeves (x1,x2,mx,f) {
                 sign=-1;
             }
             x2=x2+sign*d;
-            fX=f(x1,x2);
+            x = {x1:x1, x2: x2};
+            let fX=f.evaluate(x);
             functionIter++;
             if (fX < y){
                 y=fX;
@@ -95,7 +99,8 @@ function GetRandomBetween(min, max) {
 function SimulatedAnnealing(initialTemperature,endT,kMax,x0,y0,f,lowerBound,upperBound){
     let functionIter = 0;
     let T, currentEnergy;
-    let initialEnergy = f(x0,y0);
+    let x = {x1:x0, x2: y0};
+    let initialEnergy = f.evaluate(x);
     functionIter++;
     let minX=[];
     let minY=[];
@@ -117,7 +122,8 @@ function SimulatedAnnealing(initialTemperature,endT,kMax,x0,y0,f,lowerBound,uppe
     for (var i = 0; i<kMax;i++) {  
         x1 = GetRandomBetween(lowerX, upperX);
         y1 = GetRandomBetween(lowerY, upperY);
-        candidateEnergy=f(x1,y1);
+        x = {x1:x1, x2: y1};
+        candidateEnergy = f.evaluate(x);
         functionIter++;
         if (candidateEnergy<currentEnergy-1e-8){
             currentEnergy=candidateEnergy;
@@ -191,35 +197,16 @@ function CoordsForContour(lowerX,upperX,lowerY,upperY,f,minX,minY){
         Z[i]=new Array(quantity);
         
     }
+    let x={};
     for (let i = 0; i < quantity; i++) {
         for (let j = 0; j < quantity; j++) {
-            Z[j][i]=f(X[i],Y[j]); //rows - Yaxis, cols - Xaxis in plotly contour
+            x = {x1:X[i], x2: Y[j]};
+            Z[j][i]=f.evaluate(x);//rows - Yaxis, cols - Xaxis in plotly contour
         }
     }
     return [X,Y,Z];
 }
-function GetTargetFunction(selectedFunctionString){
-    let targetFunction;
-    if (selectedFunctionString === "default"){
-        targetFunction=DefaultFunc;
-    }
-    else {
-        if (selectedFunctionString === "bill"){
-            targetFunction=BillsFunc;
-        }
-        else {
-            if (selectedFunctionString === "vitte"){
-                targetFunction=VittesFunc;
-            }
-            else {
-                if (selectedFunctionString === "rosen"){
-                    targetFunction=RosenbrockFunc;
-                }
-            } 
-        }
-    }
-    return targetFunction;
-}
+
 function StartHJ(targetFunction,x0,y0,mx,lowerBound,upperBound){
     let x1,y1,minX,minY;
     let returned = HookeJeeves(x0,y0,mx,targetFunction);
@@ -229,12 +216,8 @@ function StartHJ(targetFunction,x0,y0,mx,lowerBound,upperBound){
     minY=returned[3];
     methodIter=returned[4];
     functionIter=returned[5];
-    let fMin=targetFunction(x1,y1); // точка глобального минимума
-    /*returned=GetAxisBounds(x0,y0,x1,y1);
-    lowerX=returned[0];
-    upperX=returned[1];
-    lowerY=returned[2];
-    upperY=returned[3];*/
+    let x = {x1:x1, x2: y1};
+    let fMin=targetFunction.evaluate(x); // точка глобального минимума
     lowerX = lowerY = lowerBound;
     upperX = upperY = upperBound;
     let X,Y,Z;
@@ -246,7 +229,8 @@ function StartHJ(targetFunction,x0,y0,mx,lowerBound,upperBound){
     for (let i = 0; i < minX.length; i++) {
         labelsList.push(i+1);
     }
-    f0 = targetFunction(x0,y0);
+    x = {x1:x0, x2: y0};
+    let f0=targetFunction.evaluate(x); 
     return [X,Y,Z,minX,minY,x1,y1,fMin,labelsList,methodIter,functionIter,f0];
 }
 function StartSA(targetFunction,x0,y0,startT,endT,maxIter,lowerBound,upperBound){
@@ -257,12 +241,9 @@ function StartSA(targetFunction,x0,y0,startT,endT,maxIter,lowerBound,upperBound)
     minY=returned[3];
     methodIter=returned[4];
     functionIter=returned[5];
-    fMin=targetFunction(x1,y1);
-    /*returned=GetAxisBounds(x0,y0,x1,y1);
-    lowerX=returned[0];
-    upperX=returned[1];
-    lowerY=returned[2];
-    upperY=returned[3];*/
+    x = {x1:x1, x2: y1};
+    fMin = targetFunction.evaluate(x);
+
     lowerX = lowerY = lowerBound;
     upperX = upperY = upperBound;
     returned=CoordsForContour(lowerX,upperX,lowerY,upperY,targetFunction,minX,minY);
@@ -273,12 +254,172 @@ function StartSA(targetFunction,x0,y0,startT,endT,maxIter,lowerBound,upperBound)
     for (let i = 0; i < minX.length; i++) {
         labelsList.push(i+1);
     }
-    f0 = targetFunction(x0,y0);
+    x = {x1:x0, x2: y0};
+    f0 = targetFunction.evaluate(x);
     return [X,Y,Z,minX,minY,x1,y1,fMin,labelsList,methodIter,functionIter,f0];   
 }
+//difference = set - subset
+function SetSubtraction(setA,setB){
+    let difference = new Set(setA);
+    for (let elem of setB){
+        if (difference.has(elem)){
+            difference.delete(elem);
+        }
+    }
+    return difference;
+}
+function SetUnion(setA, setB) {
+    let union = new Set(setA)
+    for (let elem of setB) {
+        union.add(elem)
+    }
+    return union
+}
+function ParseProduction(symbols){
+    let minStringLength = 99;
 
+    for (elem of symbols) {
+        let s = elem.length;
+        if (s < minStringLength) {
+            minStringLength = s;
+        }
+    }
+    let alphabet = new Set();
+    for (elem of symbols) {
+        if (elem.length == minStringLength) {
+            alphabet.add(elem);
+        }
+    }
+    let concatenated_symbols=SetSubtraction(symbols,alphabet);
+    let dict=new Map();
+    for (elem of concatenated_symbols){
+        let elem_old = elem;
+        let nletters = elem.length / minStringLength - 1;
+        let idx = 0;
+        for (let i = 0; i < nletters; i++) {
+            idx += minStringLength;
+            let slc = elem.slice(0,idx);
+            elem = elem.replace(slc,slc+'*');
+            idx+=1;
+        }
+        dict.set(elem_old,elem);
+    }
+    return dict;
+}
+function CreateTree(functionString) {
+    let node=math.parse(functionString);
+    let constants = new Set(['pi','e','i']);
+    let functions=new Set(['sin','cos','tan','cot','exp','abs','sqrt','sign','log']);
+    let symbols=new Set();
+    node.traverse(function (node) {
+        if (node.isSymbolNode) {
+            symbols.add(node.name);
+        }
+        else if (node.isFunctionNode && functions.has(node.name)) {
+            functions.add(node.name);
+        }
+    });
+    return [node,symbols,functions,constants];
+}
+function isAlphaNumeric(char){
+    let code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) {
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function ParseFunction(functionString) {
+    try {
+        //create tree
+        let returned = CreateTree(functionString);
+        let node = returned[0];
+        let symbols = returned[1];
+        let functions = returned[2];
+        let constants = returned[3];
+        //get names of variables
+        symbols = SetSubtraction(symbols,SetUnion(constants,functions));
+        //mapping open brackets
+        //if string contains open bracket then do mapping
+        if (functionString.indexOf('(') != -1){
+            let indexStart = 0;
+            if (functionString.charAt(0) == '('){
+                indexStart += 1;
+            }
+            let indexEnd = functionString.indexOf('(', indexStart);
+            while (indexEnd != -1){
+                let subst = functionString.substring(indexStart,indexEnd);
+                let isFunction = false;
+                for (const item of functions) {
+                    if (subst.includes(item)){
+                        isFunction = true;
+                        break;
+                    }
+                }
+                if (!isFunction && isAlphaNumeric(subst.charAt(subst.length - 1))){
+                    functionString = functionString.replace(subst,subst+'*');
+                    indexEnd += 1;
+                }
+                indexStart = indexEnd + 1;
+                indexEnd = functionString.indexOf('(', indexStart);
+            }
+            returned = CreateTree(functionString);
+            node = returned[0];
+            symbols = returned[1];
+            functions = returned[2];
+            constants = returned[3];
+            symbols = SetSubtraction(symbols,SetUnion(constants,functions));
+        }
+        //mapping and recreating the tree considering that 'xy' -> 'x*y'
+        if (symbols.size > functionDimension) {
+            var prodDict = ParseProduction(symbols);
+            for (const key of prodDict.keys()) {
+                let regex = new RegExp(key,'g');
+                functionString = functionString.replace(regex,prodDict.get(key));
+            }
+            returned = CreateTree(functionString);
+            node = returned[0];
+            symbols = returned[1];
+            functions = returned[2];
+            constants = returned[3];
+            symbols = SetSubtraction(symbols,SetUnion(constants,functions));
+        }
+        //renaming the variables to the hardcoded names 'x1','x2'
+        let symbols_sorted=Array.from(symbols).sort();
+        let symbols_new=['x1','x2'];
+        if (symbols_sorted != symbols_new){
+            let dict=new Map();
+            for (let i = 0; i < symbols_sorted.length; i++) {
+                dict.set(symbols_sorted[i],symbols_new[i]);
+            }
+            node = node.transform(function (node) {
+                if (node.isSymbolNode && symbols.has(node.name)) {
+                    return new math.SymbolNode(dict.get(node.name));
+                }
+                else {
+                    return node;
+                }
+            });
+        }
+        console.log(node.toString());
+        node.traverse(function (node) {
+            chck = node.isFunctionNode;
+            console.log(node.type, node.op, node.value,node.name,chck);
+        });
+        let compiled_node=node.compile();
+        return compiled_node;
+    }
+    catch (error) {
+        alert("ParseFunction error");
+        return 0;
+    }
+}
 function StartOptimization(selectedMethodString,selectedFunctionString,x0,y0,maxIter,lowerBound,upperBound){
-    targetFunction = GetTargetFunction(selectedFunctionString);
+    selectedFunctionString=selectedFunctionString.replace(/,/g,'.');
+    targetFunction = ParseFunction(selectedFunctionString);
     maxIter=parseInt(maxIter);
     x0=parseFloat(x0.replace(',','.'));
     y0=parseFloat(y0.replace(',','.'));
